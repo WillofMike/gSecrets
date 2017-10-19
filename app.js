8c4b10743af6315bc6cffa63ac3a8069ff2d1019
Mike <you@example.com>
2017-10-19 11:12:30 -0600

    styled and added buttons to the homepage

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 3000;
 
 const bodyParser = require('body-parser')
 
+const path = require('path')
+
 require('dotenv').config()
 
 
@@ -14,15 +16,20 @@ var jsonParser = bodyParser.json()
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'hbs');
 
 
 app.get('/', (req,res)=>{
   res.render('index',{
-    title: 'gSecrets'
+    title: 'gSecrets',
+    mtitle: 'Galvanize Secrets',
+    content: 'You must login or create an account to keep secrets!'
   })
 })
 
 app.listen(port, ()=> {
   console.log('listening on: ', port);
 })
+
+module.exports = app;
